fd14825e9ad05a894282f7c3d298109ecc129c8c
Daniel Hyres <danhyres@gmail.com>
2023-05-09 16:35:34 -0400

    Adding components

---
+++ components/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+function Layout(props) {
+  return (
+    <div className='site-wrapper'>Lay</div>
+  )
+}
+
+
+export default Layout
