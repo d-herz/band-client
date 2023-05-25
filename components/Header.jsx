8afe625acad66ee63442542bd34a56fcbb85b348
Daniel Hyres <danhyres@gmail.com>
2023-05-25 12:30:17 -0400

    Brain Slug site updates

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,21 +2,32 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
 
 function Header() {
+
+  const activeStyle = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#535bf2"
+  }
+
   return (
     <header>
+      <Link className='site-logo' to="/">#BRAINSLUGCT</Link>
       <nav>
         <NavLink
           to="/"
+          style={ ({isActive}) => isActive ? activeStyle : null }
         >
           Home
         </NavLink>
         <NavLink
           to="/about"
+          style={ ({isActive}) => isActive ? activeStyle : null }
         >
           About
         </NavLink>
         <NavLink
           to="/shows"
+          style={ ({isActive}) => isActive ? activeStyle : null }
         >
           Shows
         </NavLink>
