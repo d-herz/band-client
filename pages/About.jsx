c4c9c5f57a7113bfc520358a94e06485a859de17
Daniel Hyres <danhyres@gmail.com>
2023-05-28 16:31:57 -0400

    Brain Slug updates

--- pages/About.jsx
+++ pages/About.jsx
@@ -8,16 +8,16 @@ function About() {
       <div className="about-page-content">
         <h1>POP PUNK's NOT DEAD</h1>
         <p>
-          Introducing Brain Slug! The mischievous mavericks of New England's pop-punk scene! Hailing from the land of clam chowder and revolutionary spirit, this energetic quartet is ready to rock your world, and possibly rearrange your brain cells in the process. With a sound that's punchier than a lobster roll and catchier than a Red Sox victory chant, Brain Slug's music will invade your eardrums like an unstoppable earworm.
+          Introducing Brain Slug! The mischievous mavericks of Connecticut's pop-punk scene! Hailing from the wild realms of Torrington, Winsted, and everywhere in between, this electrifying quartet is ready to rock your world, and possibly rearrange your brain cells in the process.  Their music hits harder than rush hour traffic on Route 8 and sticks to your eardrums like the finest single mom's secret recipe. 
         </p>
         <p>
-          These rebels with guitar straps are known for their irreverent antics both on and off the stage. If you ever catch them backstage, don't be surprised to find them trading guitar picks for lobsters, or discussing the intricacies of punk philosophy with a side of maple syrup-soaked pancakes. Brain Slug's hilarious banter and infectious humor will have you laughing, moshing, and wondering why your face hurts from smiling so much.
+          These rebels with guitar straps are known for their irreverent antics both on and off the stage. If you ever catch them backstage, don't be surprised to find them trading guitar picks for pizza slices, or discussing the intricacies of punk philosophy with the local wildlife. Brain Slug's hilarious banter and infectious humor will have you laughing, moshing, and wondering why your face hurts from smiling so much.
         </p>
         <p>
           Led by their charismatic frontperson, whose hair is as wild as a moose on a caffeine bender, Brain Slug fearlessly tackles the absurdities of life with their lyrical prowess. From poking fun at the woes of adulthood to questioning the cosmic significance of pizza toppings, they embrace irreverence as their musical religion. Their songs are rebellious anthems that unite the disenchanted youth and anyone else who appreciates a good riff and a hearty laugh.
         </p>
         <p>
-          So, join Brain Slug's raucous revolution, because life's too short to take seriously. Jump, scream, and let loose with the band that will leave you buzzing like a bee on a sugar high. Prepare for pop-punk pandemonium and a rollicking good time, courtesy of Brain Slug, New England's ultimate punk rock jesters. Embrace the mayhem, or risk missing out on a wild ride that will make you question everything, including your choice of breakfast cereal.
+          So, join Brain Slug's raucous revolution, because life's too short to take seriously. Jump, scream, and let loose with the band that will leave you buzzing like a bee on a sugar high. Get ready for a whirlwind of pop-punk chaos, where Torrington and Winsted become the backdrop for unforgettable musical adventures. Embrace the mayhem, or risk missing out on a wild ride that will make you question everything, including your choice of late-night pizza joint.
         </p>
 
       </div>
@@ -26,11 +26,10 @@ function About() {
         <h3>MEMBERS</h3>
 
       </div>
-      <div className="about-page-cta">
+      <div className="about-page-content">
         <h2>
-          Your destination is waiting. <br />Your van is ready.
+          <Link to="/">#BRAINSLUGCT</Link>
         </h2>
-        <Link className='link-button' to="/shows">Shows</Link>
       </div>
     </div>
   )
--- pages/Shows.jsx
+++ pages/Shows.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
 
 function Schedule() {
+
+  const shows = [
+    
+    {
+      date: 'June 23, 2023',
+      venue: '924 Gilman Street',
+      location: 'Berkeley, CA',
+      openingActs: ['Operation Ivy','Green Day'],
+      soldOut: true,
+    },
+    {
+      date: 'June 30, 2023',
+      venue: 'The Greek Theater',
+      location: 'Los Angeles, CA',
+      openingActs: ['Blink 182'],
+      soldOut: true,
+    },
+    {
+      date: 'July 7th, 2023',
+      venue: 'Madison Square Garden',
+      location: 'New York City, NY',
+      openingActs: ['Nofx', 'The Offspring'],
+      soldOut: true,
+    },
+    // Add more show objects as needed
+  ];
+
   return (
-    <div>Schedule</div>
+    <div className="show-page-container">
+
+      <div className="show-page-content">
+        <h1>Upcoming Shows</h1>
+        <ul>
+          {shows.map((show, index) => (
+            <li key={index}>
+              <strong>{show.date}</strong> - {show.venue}, {show.location}
+              {show.openingActs.length > 0 && (
+                <div className='opening-acts'>
+                  <span>Opening Acts:</span>  {show.openingActs.join(', ')}
+                </div>
+              )}
+              {show.soldOut && <span className='ticket-status'> - Sold Out</span>}
+            </li>
+          ))}
+        </ul>
+       
+
+      </div>
+
+      <div className="about-page-content">
+        <h2>
+
+        </h2>
+      </div>
+    </div>
   )
 }
 
