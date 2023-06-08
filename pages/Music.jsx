f3ecb6266f44fe70bd08d20cb6519fce149bf50a
Daniel Hyres <danhyres@gmail.com>
2023-06-07 20:34:02 -0400

    Music component updates

--- pages/Music.jsx
+++ pages/Music.jsx
@@ -5,7 +5,16 @@ function Music() {
     <div className="show-page-container">
 
       <div className="show-page-content">
-        
+        <h3>Streaming Platforms</h3>
+        <ul>
+          <li>
+            <a target="_blank" href="https://music.apple.com/us/artist/brain-slug/1650056054">Apple Music</a>
+          </li>
+          <li>
+            <a target="_blank" href="https://open.spotify.com/artist/2Zz8hdMk5BQFlOkrGf2EBs">Spotify</a>
+          </li>
+          
+        </ul>
 
 
       </div>
