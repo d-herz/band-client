d9ced194b5f0a70f1686052ff800b9cdb008204d
Daniel Hyres <danhyres@gmail.com>
2023-06-09 21:17:56 -0400

    Updates to home page

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// import bandImage from '/public/images/gig-concert-show-music.jpg' 
 
 function Home() {
   return (
-    <div className='home-container'>
-      <h1>Brain Slug!</h1>
-      <h2>Rocking New England's Punk Scene, One Slug at a Time!</h2>
-      
-      <span>
-        "My favorite band!" - Elliot, age 2
-      </span>
+    <>
+      <div className='home-container-hero'>
 
-      <p>
-        Add adventure to your life by joining the #BrainSlug movement 
-      </p>
-      <Link to="shows">Upcoming Shows</Link>
-    </div>
+        <h1>Brain Slug!</h1>
+          
+      </div>
+
+      <div className='home-container-main'>
+        <h2>Rocking New England's Punk Scene, One Slug at a Time!</h2>
+
+        <span>
+          "My favorite band!" - Elliot, age 2
+        </span>
+
+        <p>
+          Add adventure to your life by joining the #BrainSlug movement
+        </p>
+        <Link to="shows">Upcoming Shows</Link>
+      </div>
+    </>
     
   )
 }
